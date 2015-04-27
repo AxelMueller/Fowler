0337a9fe1967621c015a522fec0b8f9247fc43ee
swruk <swruk@SWruk.abas-projektierung.local>
2015-04-27 09:51:49 +0200

    Moved getCharge and getRentalpoints to price classes

--- src/Movie.java
+++ src/Movie.java
@@ -3,10 +3,7 @@ public class Movie {
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
 	private String title;
-	private int priceCode;
 	private Price _price;
-
-
 
 	public Movie(String newtitle, int newpriceCode) {
 		title = newtitle;
@@ -18,34 +15,13 @@ public class Movie {
 	}
 
 	double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
-			break;
-		}
-		return result;
-	};
-
-	public int getFrequentRenterPoints(int daysRented) {
-		// add bonus for a two day new release rental
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-			return 2;
-		} else {
-			return 1;
-		}
+		return _price.getCharge(daysRented);
 	}
-	
+
+	int getFrequentRenterPoints(int daysRented) {
+		return _price.getFrequentRenterPoints(daysRented);
+	}
+
 	public int getPriceCode() {
 		return _price.getPriceCode();
 	}
